b7fae6781e7867adefbca6403e5d76e4beb3b5b2
VladaM <vladaa905@gmail.com>
2025-05-07 17:49:38 +0300

    fix linter feedback

--- django_recipe_generator/recipe_generator/api/urls.py
+++ django_recipe_generator/recipe_generator/api/urls.py
@@ -1,22 +1,21 @@
-from django.urls import path, include
+from django.urls import path
+from django.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from django_recipe_generator.recipe_generator.api import views
-from rest_framework.authtoken.views import obtain_auth_token
 
 
 router = DefaultRouter()
 router.register(r'recipes', views.RecipeViewSet, basename='recipe')
-'''
-urlpatterns = [
-    path('', include(router.urls)),
-]
-'''
+
 urlpatterns = [
     path('', include([
-        path('', views.RecipeViewSet.as_view({'get': 'api_root'}), name='api-root'),
+        path('', views.RecipeViewSet.as_view({'get': 'api_root'}),
+             name='api-root'),
         path('', include(router.urls)),
-        path('recipe-form-data/', views.RecipeFormDataView.as_view(), name='recipe-form-data'),
-        path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+        path('recipe-form-data/', views.RecipeFormDataView.as_view(),
+             name='recipe-form-data'),
+        path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
         path('register/', views.RegisterView.as_view(), name='register'),
     ])),
 ]
@@ -32,5 +31,9 @@ GET /api/recipes/1/ → Retrieve a recipe
 
 PUT /api/recipes/1/ → Update a recipe
 
-DELETE /api/recipes/1/ → Delete a recipe +
+DELETE /api/recipes/1/ → Delete a recipe
+
++
+
+POST /filter_search/ → Filter/serach recipe
 '''
--- django_recipe_generator/recipe_generator/api/views.py
+++ django_recipe_generator/recipe_generator/api/views.py
@@ -1,18 +1,24 @@
 from rest_framework.response import Response
-from django_recipe_generator.recipe_generator.models import Recipe, Ingredient
-from django_recipe_generator.recipe_generator.serializers import RecipeSerializer, IngredientSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.decorators import action
 from rest_framework.reverse import reverse
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import permission_classes
+
+from django_recipe_generator.recipe_generator.models import Recipe, Ingredient
+from django_recipe_generator.recipe_generator.serializers import (
+    RecipeSerializer,
+    IngredientSerializer,
+    UserSerializer
+)
+
 from django.contrib.auth.models import User
 
 
-   
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-
 
     @action(detail=False, methods=['get'])
     def api_root(self, request):
@@ -24,10 +30,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
             },
             'recipes_create': {
                 'url': reverse('recipe-list', request=request),
-                'method': ['POST'], 
+                'method': ['POST'],
             },
         })
-    
+
     @action(detail=False, methods=['POST'])
     def filter_search(self, request):
         query_name = request.data.get('query_name', '')
@@ -35,33 +41,65 @@ class RecipeViewSet(viewsets.ModelViewSet):
         query_ingredients = request.data.get('query_ingredients', [])
         exclude_ingredients = request.data.get('exclude_ingredients', [])
 
-        qs = Recipe.objects.search(query_name=query_name,query_ingredients=query_ingredients).filter_recipes(time_filter=time_filter,
-            exclude_ingredients=exclude_ingredients).prefetch_related('ingredients')
-        
+        qs = Recipe.objects.search(
+            query_name=query_name,
+            query_ingredients=query_ingredients
+        ).filter_recipes(
+            time_filter=time_filter,
+            exclude_ingredients=exclude_ingredients
+        ).prefetch_related('ingredients')
+
         if query_ingredients:
-            ingredient_lookup_query = {i.id: i.name for i in Ingredient.objects.filter(id__in=query_ingredients)}
+            ingredient_lookup_query = {
+                i.id: i.name
+                for i in Ingredient.objects.filter(id__in=query_ingredients)
+            }
 
             for recipe in qs:
-                recipe_ingredient_ids = set(recipe.ingredients.values_list('id', flat=True))
-                matching_ids = recipe_ingredient_ids.intersection(set(query_ingredients))
-                missing_ids = set(recipe_ingredient_ids) - set(query_ingredients)
+                recipe_ingredient_ids = set(
+                    recipe.ingredients.values_list('id', flat=True)
+                )
+                matching_ids = recipe_ingredient_ids.intersection(
+                    set(query_ingredients)
+                )
+                missing_ids = set(recipe_ingredient_ids) - set(
+                    query_ingredients
+                )
 
-                ingredient_lookup_recipe = {i.id: i.name for i in Ingredient.objects.filter(id__in=missing_ids)}
+                ingredient_lookup_recipe = {
+                    i.id: i.name
+                    for i in Ingredient.objects.filter(id__in=missing_ids)
+                }
 
                 recipe.matching_ingredient_ids = list(matching_ids)
                 recipe.missing_ingredient_ids = list(missing_ids)
 
-                recipe.matching_ingredient_names = [ingredient_lookup_query[i] for i in matching_ids]
-                recipe.missing_ingredient_names = [ingredient_lookup_recipe[i] for i in missing_ids]
+                recipe.matching_ingredient_names = [
+                    ingredient_lookup_query[i]
+                    for i in matching_ids
+                ]
+                recipe.missing_ingredient_names = [
+                    ingredient_lookup_recipe[i]
+                    for i in missing_ids
+                ]
 
         page = self.paginate_queryset(qs)
         if page is not None:
-            serializer = self.get_serializer(page, many=True, context={'include_ingredient_analysis': True})
+            serializer = self.get_serializer(
+                page,
+                many=True,
+                context={'include_ingredient_analysis': True}
+            )
             return self.get_paginated_response(serializer.data)
 
-        serializer = self.get_serializer(qs, many=True, context={'include_ingredient_analysis': True})
-        
+        serializer = self.get_serializer(
+            qs,
+            many=True,
+            context={'include_ingredient_analysis': True}
+        )
+
         return Response(serializer.data)
+
 
 class RecipeFormDataView(generics.ListAPIView):
     """
@@ -71,7 +109,7 @@ class RecipeFormDataView(generics.ListAPIView):
     serializer_class = IngredientSerializer
 
 
+@permission_classes([AllowAny])
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
