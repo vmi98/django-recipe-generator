392fa510baa5367d83ce2b9126e6f1035856a8a6
VladaM <vladaa905@gmail.com>
2025-05-15 11:48:45 +0300

    fix for deploy

--- django_recipe_generator/settings.py
+++ django_recipe_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,7 @@ REST_FRAMEWORK = {
 }
 
 INTERNAL_IPS = ['127.0.0.1', '0.0.0.0']
+
+if os.environ.get("DJANGO_ENV") != "production":
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
