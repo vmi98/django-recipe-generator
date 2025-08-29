c05a346fac78d0ddbf549a8f6d5a14f40c192855
VladaM <vladaa905@gmail.com>
2025-08-29 14:20:37 +0300

    refactor: add custom permission

---
+++ django_recipe_generator/recipe_generator/api/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOrAdmin(BasePermission):
+    """
+    Custom permission: Only the owner or an admin can edit/delete.
+    Others can only read.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.owner == request.user or request.user.is_staff
